49d38debf809a7fa7f4a61e393caf6e904ae12e4
Kmenguete <menguetekevin@gmail.com>
2022-11-15 10:38:12 +0100

    writing unit tests for the first bug

--- points_display_board_test/points_display_board_unit_test.py
+++ points_display_board_test/points_display_board_unit_test.py
@@ -23,3 +23,18 @@ def test_create_club(mocker):
 def test_status_code_ok_index(client):
     response = client.get('/index')
     assert response.status_code == 200
+
+
+def test_should_access_to_welcome_page(client):
+    email = "example@gmail.com"
+    response = client.post('/show_summary',
+                           data={"email": email},
+                           follow_redirects=True)
+    assert response.status_code == 200
+    data = response.data.decode()
+    assert data.find("Welcome to the GUDLFT Registration Portal!") == -1
+
+
+def test_should_access_to_points_boards(client):
+    response = client.get('/points_board')
+    assert response.status_code == 200
