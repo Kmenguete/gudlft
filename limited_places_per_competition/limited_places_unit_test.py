28eac13361903a854edbfbaf6301d56622eb3bb6
Kmenguete <menguetekevin@gmail.com>
2022-11-28 13:49:17 +0100

    a club should not be able to book more than 12 places per competition

---
+++ limited_places_per_competition/limited_places_unit_test.py
@@ -0,0 +1,110 @@
+from main import create_club
+
+from main import create_competition
+
+import server
+
+
+class ClubMockResponse:
+
+    @staticmethod
+    def get_info():
+        return {"name": "Club Test",
+                "email": "example@gmail.com",
+                "points": "20"}
+
+    def __getitem__(self, item):
+        return self.get_info()
+
+
+class CompetitionMockResponse:
+
+    @staticmethod
+    def get_info():
+        return {"name": "Competition Test",
+                "date": "2023-06-09 10:00:00",
+                "numberOfPlaces": "50"}
+
+
+def test_create_club(monkeypatch):
+    def mock_get(*args, **kwargs):
+        return ClubMockResponse()
+
+    monkeypatch.setattr('main.Club', mock_get)
+
+    expected_value = {"name": "Club Test",
+                      "email": "example@gmail.com",
+                      "points": "20"}
+    assert create_club() == expected_value
+
+
+def test_create_competition(monkeypatch):
+    def mock_get(*args, **kwargs):
+        return CompetitionMockResponse()
+
+    monkeypatch.setattr('main.Competition', mock_get)
+
+    expected_value = {"name": "Competition Test",
+                      "date": "2023-06-09 10:00:00",
+                      "numberOfPlaces": "50"}
+    assert create_competition() == expected_value
+
+
+def test_status_code_ok_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_should_access_to_welcome_page(client, mocker):
+    mocker.patch.object(server, 'clubs', [{"name": "Club Test",
+                                           "email": "example@gmail.com",
+                                           "points": "20"}])
+    club = "example@gmail.com"
+    response = client.post('/show_summary',
+                           data={"email": club},
+                           follow_redirects=True)
+    assert response.status_code == 200
+    data = response.data.decode()
+    assert data.find("Welcome to the GUDLFT Registration Portal!") == -1
+
+
+def _should_find_competition_and_club(client, competition, club):
+    response = client.get(f'/book/{competition}/{club}')
+    assert competition == 'Competition Test'
+    assert club == 'Club Test'
+    assert response.status_code == 200
+
+
+def test_should_access_to_book_places_page(client, mocker):
+    mocker.patch.object(server, 'clubs', [{"name": "Club Test",
+                                           "email": "example@gmail.com",
+                                           "points": "20"}])
+    mocker.patch.object(server, 'competitions', [{"name": "Competition Test",
+                                                  "date": "2023-06-09 10:00:00",
+                                                  "numberOfPlaces": "50"}])
+    club = 'Club Test'
+    competition = 'Competition Test'
+    _should_find_competition_and_club(client, competition, club)
+
+
+def _purchase_places(client, club, competition, places):
+    response = client.post('/purchase_places',
+                           data={"club": club['name'],
+                                 "competition": competition['name'],
+                                 "places": places},
+                           follow_redirects=True)
+    assert response.status_code == 400
+    data = response.data.decode()
+    assert data.find("{{competition['name']}}") == -1
+
+
+def test_should_not_book_more_than_12_places(client, mocker):
+    clubs = mocker.patch.object(server, 'clubs', [{"name": "Club Test",
+                                                   "email": "example@gmail.com",
+                                                   "points": "20"}])
+    competitions = mocker.patch.object(server, 'competitions', [{"name": "Competition Test",
+                                                                 "date": "2023-06-09 10:00:00",
+                                                                 "numberOfPlaces": "50"}])
+    club = [club for club in clubs][0]
+    competition = [competition for competition in competitions][0]
+    _purchase_places(client, club, competition, 16)
