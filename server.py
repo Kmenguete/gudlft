3742770ece062ee50292b8ed4fafefa15aac7965
Kmenguete <menguetekevin@gmail.com>
2022-11-29 16:56:56 +0100

    successfully prevented unauthenticated users to access to the booking page

--- server.py
+++ server.py
@@ -40,18 +40,23 @@ def show_summary():
 
 @app.route('/book/<competition>/<club>', methods=['GET'])
 def book(competition, club):
-    found_club = [c for c in clubs if c['name'] == club][0]
-    found_competition = [c for c in competitions if c['name'] == competition][0]
-    if found_club and found_competition:
-        if datetime.strptime(found_competition['date'], '%Y-%m-%d %H:%M:%S') < datetime.now():
-            response = make_response("<p>You cannot book places in a past competition<p>")
-            response.status_code = 400
-            return response
+    try:
+        found_club = [c for c in clubs if c['name'] == club][0]
+        found_competition = [c for c in competitions if c['name'] == competition][0]
+        if found_club and found_competition:
+            if datetime.strptime(found_competition['date'], '%Y-%m-%d %H:%M:%S') < datetime.now():
+                response = make_response("<p>You cannot book places in a past competition<p>")
+                response.status_code = 400
+                return response
+            else:
+                return render_template('booking.html', club=club, competition=competition)
         else:
-            return render_template('booking.html', club=club, competition=competition)
-    else:
-        flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+            flash("Something went wrong-please try again")
+            return render_template('welcome.html', club=club, competitions=competitions)
+    except IndexError:
+        response = make_response("<p>You did not provide the necessary credentials to access this page.<p>")
+        response.status_code = 401
+        return response
 
 
 @app.route('/purchase_places', methods=['POST'])
