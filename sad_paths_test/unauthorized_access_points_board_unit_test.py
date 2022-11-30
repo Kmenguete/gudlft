7bd4f62aca763abb53d8eabe19dbda2cbc3fb889
Kmenguete <menguetekevin@gmail.com>
2022-11-30 16:53:27 +0100

    unauthenticated user successfully failed to access points board page

---
+++ sad_paths_test/unauthorized_access_points_board_unit_test.py
@@ -0,0 +1,9 @@
+import server
+
+
+def test_should_not_access_to_points_boards(client, mocker):
+    mocker.patch.object(server, 'clubs', [])
+    club = None
+    response = client.get('/points_board')
+    assert club is None
+    assert response.status_code == 401
