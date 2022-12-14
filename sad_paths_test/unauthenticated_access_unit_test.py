8084e61d0f7e8af745c945c4693436142402aec3
Kevin Menguete <73162874+Kmenguete@users.noreply.github.com>
2022-12-14 11:08:05 +0100

    Delete sad_paths_test directory
    This directory already exists in tests directory.

--- sad_paths_test/unauthenticated_access_unit_test.py
+++
@@ -1,9 +0,0 @@
-import server
-
-
-def test_should_not_access_to_welcome_page(client, mocker):
-    mocker.patch.object(server, 'clubs', [])
-    club = None
-    response = client.get('/back_to_welcome_page')
-    assert club is None
-    assert response.status_code == 401
--- sad_paths_test/unauthorized_access_points_board_unit_test.py
+++
@@ -1,9 +0,0 @@
-import server
-
-
-def test_should_not_access_to_points_boards(client, mocker):
-    mocker.patch.object(server, 'clubs', [])
-    club = None
-    response = client.get('/points_board')
-    assert club is None
-    assert response.status_code == 401
